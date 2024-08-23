ea46bf8ddc2871a69b5bda2b6c4cfeec87f4c1db
Jitesh Yadav <jiteshyadav@Jiteshs-Laptop.attlocal.net>
2024-08-23 01:24:54 -0400

    fix: minor fix

--- apps/user-web/src/app/store/page.tsx
+++ apps/user-web/src/app/store/page.tsx
@@ -4,9 +4,17 @@ import { useSearchParams } from "next/navigation";
 import { useRouter } from "next/navigation";
 import { type Store } from "@repo/ui/types";
 import { StorePage } from "@repo/ui/user/pages";
-import { useEffect } from "react";
+import { Suspense, useEffect } from "react";
 
-export default function Store() {
+export default function StoreContainer() {
+  return (
+    <Suspense>
+      <Store />
+    </Suspense>
+  );
+}
+
+function Store() {
   const router = useRouter();
   const searchParams = useSearchParams();
   const storeId = searchParams?.get("id");
