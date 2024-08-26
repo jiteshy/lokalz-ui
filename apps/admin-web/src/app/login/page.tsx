6641ffed39a814c7c866f2661051e76de913deb9
Jitesh Yadav <jiteshyadav@Jiteshs-Laptop.attlocal.net>
2024-08-25 23:11:22 -0400

    feat: add google sign in

--- apps/admin-web/src/app/login/page.tsx
+++ apps/admin-web/src/app/login/page.tsx
@@ -1,51 +1,49 @@
-"use client";
-
-import { authenticate } from "@/lib/actions";
-import { useFormState } from "react-dom";
+import { signIn } from "@/auth";
 
 export default function LoginPage() {
-  const [errorMessage, formAction, isPending] = useFormState(
-    authenticate,
-    undefined,
-  );
-
   return (
-    <div className="min-h-screen flex justify-center items-start md:items-center p-8 bg-slate-100">
-      <div className="w-full max-w-sm bg-white border border-slate-300 rounded-lg p-8">
-        <div>
-          <h2 className="text-2xl text-slate-800">Login</h2>
-          <span className="text-sm text-slate-500">
-            Provide the key phrase for admin access
-          </span>
-        </div>
-        <div className="">
-          <div className="text-sm text-red-accent-700 h-8" aria-live="polite">
-            {errorMessage && <p>{errorMessage}</p>}
+    <div className="min-h-screen flex justify-center items-start md:items-center p-8 bg-white">
+      <form action={async () => {
+        "use server";
+        await signIn("google");
+      }}>
+        <button
+          type="submit"
+          className="flex items-center justify-center gap-3 cursor-pointer rounded-full bg-slate-900 px-3 py-3 text-sm drop-shadow-md hover:bg-slate-800"
+        >
+          <GoogleLogo />
+          <div className="text-lg text-slate-100 font-light">
+            Sign in with Google
           </div>
-          <form
-            action={formAction}
-            className="w-full flex items-center justify-between gap-3"
-          >
-            <input
-              type="password"
-              name="keyPhrase"
-              required
-              placeholder="Enter Key Phrase"
-              className={`${
-                errorMessage ? "border-red-accent-400" : "border-slate-400"
-              } px-3 py-2 w-3/4 text-slate-900 transition duration-200 bg-white border rounded shadow-sm appearance-none focus:outline-none`}
-            />
-            <button
-              type="submit"
-              aria-disabled={isPending}
-              disabled={isPending}
-              className="w-1/4 px-3 py-2 bg-slate-700 text-slate-200 rounded hover:bg-slate-900 disabled:bg-slate-400"
-            >
-              Login
-            </button>
-          </form>
-        </div>
-      </div>
+        </button>
+      </form>
     </div>
   );
 }
+
+const GoogleLogo = () => (
+  <svg
+    width="24"
+    height="24"
+    viewBox="0 0 775 794"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M775 405.797C775 373.248 772.362 349.496 766.653 324.865H395.408V471.773H613.32C608.929 508.282 585.204 563.264 532.482 600.209L531.743 605.127L649.124 696.166L657.256 696.979C731.943 627.921 775 526.315 775 405.797"
+      fill="#4285F4"
+    />
+    <path
+      d="M395.408 792.866C502.167 792.866 591.792 757.676 657.256 696.979L532.482 600.209C499.093 623.521 454.279 639.796 395.408 639.796C290.845 639.796 202.099 570.741 170.463 475.294L165.826 475.688L43.772 570.256L42.1759 574.698C107.198 704.013 240.758 792.866 395.408 792.866Z"
+      fill="#34A853"
+    />
+    <path
+      d="M170.463 475.294C162.116 450.662 157.285 424.269 157.285 397C157.285 369.728 162.116 343.338 170.024 318.706L169.803 313.46L46.2193 217.373L42.1759 219.299C15.3772 272.961 0 333.222 0 397C0 460.778 15.3772 521.036 42.1759 574.698L170.463 475.294"
+      fill="#FBBC05"
+    />
+    <path
+      d="M395.408 154.201C469.656 154.201 519.74 186.31 548.298 213.143L659.891 104.059C591.356 40.2812 502.167 1.13428 395.408 1.13428C240.758 1.13428 107.198 89.9835 42.1759 219.299L170.024 318.706C202.099 223.259 290.845 154.201 395.408 154.201"
+      fill="#EB4335"
+    />
+  </svg>
+);
--- apps/admin-web/src/auth.ts
+++ apps/admin-web/src/auth.ts
@@ -1,31 +1,37 @@
 import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
+import GoogleProvider from "next-auth/providers/google";
+
 import { authConfig } from "./auth.config";
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
   ...authConfig,
   providers: [
-    CredentialsProvider({
-      // The name to display on the sign in form (e.g. "Sign in with...")
-      name: "Credentials",
-      // `credentials` is used to generate a form on the sign in page.
-      // You can specify which fields should be submitted, by adding keys to the `credentials` object.
-      // e.g. domain, username, password, 2FA token, etc.
-      // You can pass any HTML attribute to the <input> tag through the object.
-      credentials: {
-        keyPhrase: {
-          label: "Key Phrase",
-          type: "password",
-          placeholder: "Enter key phrase",
-        },
-      },
-      async authorize(credentials) {
-        if (process.env.AUTH_KEY_PHRASE === credentials?.keyPhrase) {
-          return { id: "admin", name: "Admin" };
-        } else {
-          return null;
-        }
-      },
-    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET
+    }),  
+    // CredentialsProvider({
+    //   // The name to display on the sign in form (e.g. "Sign in with...")
+    //   name: "Credentials",
+    //   // `credentials` is used to generate a form on the sign in page.
+    //   // You can specify which fields should be submitted, by adding keys to the `credentials` object.
+    //   // e.g. domain, username, password, 2FA token, etc.
+    //   // You can pass any HTML attribute to the <input> tag through the object.
+    //   credentials: {
+    //     keyPhrase: {
+    //       label: "Key Phrase",
+    //       type: "password",
+    //       placeholder: "Enter key phrase",
+    //     },
+    //   },
+    //   async authorize(credentials) {
+    //     if (process.env.AUTH_KEY_PHRASE === credentials?.keyPhrase) {
+    //       return { id: "admin", name: "Admin" };
+    //     } else {
+    //       return null;
+    //     }
+    //   },
+    // }),
   ],
 });
