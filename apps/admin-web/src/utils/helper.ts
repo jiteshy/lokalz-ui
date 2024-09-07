4d02ed98365a364ebb7e99219959a95ebe771a57
Jitesh Yadav <jiteshyadav@Jiteshs-Laptop.attlocal.net>
2024-09-07 01:18:43 -0400

    fix: store type fix

--- apps/admin-web/src/utils/helper.ts
+++ apps/admin-web/src/utils/helper.ts
@@ -6,6 +6,12 @@ import axios from "@/lib/axios-client";
 export const getUserFromDB = async (
   user: Partial<User>,
 ): Promise<CustomUser> => {
-  const response = await axios.post("/auth/login/callback", user);
-  return response.data;
+  // TO-DO: Not working after deployment without try catch
+  try {
+    const response = await axios.post("/auth/login/callback", user);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+    return {accessToken: '', role: '', token: ''};
+  }
 };
