ce5e477aa5a631c9ac926b4ef638d358581d2a34
Jitesh Yadav <jiteshyadav@Jiteshs-Laptop.attlocal.net>
2024-10-04 00:08:23 -0400

    fix: publish store bg color change

--- apps/admin-web/src/components/store/publish/store-publish.tsx
+++ apps/admin-web/src/components/store/publish/store-publish.tsx
@@ -51,7 +51,7 @@ export const StorePublish = ({
         </h4>
         <div className="flex items-center gap-3">
           {storeStatus !== StoreStatus.ACTIVE ? (
-            <Button type="submit" onClick={handlePublishStore}>
+            <Button type="submit" className="bg-deep-purple-accent-400" onClick={handlePublishStore}>
               <CheckIcon className="w-5 h-5" />
               <span className="pl-2">Publish Store</span>
             </Button>
